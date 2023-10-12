c6565be640071d206621a27a3c9d1a8f174c97f2
Jais Jose <jaisjose2001@gmail.com>
2023-10-12 15:08:57 +0530

    Update SPI.py

--- SPI.py
+++ SPI.py
@@ -3,13 +3,17 @@ import spidev
 from time import sleep
 
 spi = spidev.SpiDev()  #enable spi
-CS = 0 #pin for chip select cs is active low
-spi.open(0, CS)#open and initialize the SPI communication with a specific SPI device connected to the SPI bus.
-# bus port 0 and cs pin to establish spi
-spi.max_speed_hz = 4300000# MAX6675(ADC) is of 4.3MHz 
+CS_Pin = 23
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(CS_Pin,GPIO.out)
+result = 0x0000
+spi.open(0)#open and initialize the SPI communication with a specific SPI device connected to the SPI bus.
+# bus port 0 to establish spi
+spi.max_speed_hz = 1000# MAX6675(ADC) is of 4.3MHz 
 spi.mode = 0
-data= 0x23
-spi.xfer2(data)
-sleep(2)
+GPIO.output(CS_Pin,GPIO.LOW)
+spi.writebytes(0)
+spi.writebytes (0)
+GPIO.output(CS_Pin,GPIO.HIGH)
 print("i finished transmitting")
 
